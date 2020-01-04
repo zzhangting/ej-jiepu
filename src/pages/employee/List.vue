9426a967800daecd8c17ffd5a6534656f9084b84
yyangxiaoru <yyangxiaoru@126.com>
2020-01-04 20:30:50 +0800

    yxr提交

--- src/pages/employee/List.vue
+++ src/pages/employee/List.vue
@@ -1,5 +1,171 @@
 <template>
-<div>
-    员工管理
-</div>
+  <div>
+    <!-- 按钮 -->
+    <div>
+      <el-button size="small" type="primary" @click="toAddHandler">添加</el-button>
+      <el-button size="small" type="danger">批量删除</el-button>
+    </div>
+    <!-- /按钮 -->
+    <!-- 表格 -->
+    <el-table :data="employees">
+      <el-table-column fixed="left" prop="id" label="编号"></el-table-column>
+      <el-table-column  fixed="left" prop="username" label="真实姓名"></el-table-column>
+      <el-table-column  prop="realname" label="姓名"></el-table-column>
+      <el-table-column prop="gender" label="性别"></el-table-column>
+      <el-table-column width="120" prop="telephone" label="手机号"></el-table-column>
+      <el-table-column width="200" prop="idCard" label="身份证号"></el-table-column>
+      <el-table-column width="200" prop="bankCard" label="银行卡号"></el-table-column>
+      <el-table-column fixed="right" label="操作">
+        <template v-slot="slot">
+          <!-- {[slot.row]}  -->
+          <a href="" @click.prevent="toDeleteHandler(slot.row.id)">删除</a>
+          <a href="" @click.prevent="toUpdateHandler(slot.row)">修改</a>
+        </template>
+      </el-table-column>
+    </el-table>
+    <!-- /表格结束 -->
+    <!-- 分页开始 -->
+    <el-pagination layout="prev, pager, next" :total="50"></el-pagination>
+    <!-- /分页结束 -->
+    <!-- 模态框 -->
+   <el-dialog 
+        :title="title" 
+        :visible.sync="visible" 
+        width ="60%">
+        测试:{{form}}
+            <el-form  :model="form" label-width="80px">
+              <el-form-item label="用户名">
+                  <el-input v-model="form.username"/>
+              </el-form-item>
+              <el-form-item label="密码">
+                  <el-input v-model="form.password" type="password"/>
+              </el-form-item>
+              <el-form-item label="姓名">
+                  <el-input v-model="form.realname"/> 
+              </el-form-item>
+              <el-form-item label="性别">
+                  <el-radio-group v-model="form.gender">
+                         <el-radio label="男">男</el-radio>
+                         <el-radio label="女">女</el-radio>
+                  </el-radio-group>
+              </el-form-item>
+              <el-form-item label="手机号">
+                  <el-input v-model="form.telephone"/> 
+              </el-form-item>
+              <el-form-item label="身份证号">
+                  <el-input v-model="form.idCard"/> 
+              </el-form-item>
+              <el-form-item label="银行卡号">
+                  <el-input v-model="form.bankCard"/> 
+              </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button size="small" @click="closeModalHandler">
+                    取消
+                </el-button>
+                <el-button size="small" type="primary" @click="sumbitHandler">
+                    确定
+                </el-button>    
+            </span>
+
+        </el-dialog>
+    <!-- /模态框 -->
+
+  </div>
 </template>
+
+<script>
+import request from '@/utils/request' //自定义库
+import querystring from 'querystring' //系统库不需加路径
+export default{
+  data(){
+    return {
+      
+      visible:false,
+      employees:[],
+      form:{
+        type:"waiter"
+
+      }
+    }
+  },
+  created(){
+    this.loadData();
+    //在页面加载出来时加载数据
+  },
+  methods:{
+    sumbitHandler(){
+      let url = "http://localhost:6677/waiter/saveOrUpdate"
+      request({
+        url,
+        method:"post",
+        headers:{
+          "Content-Type":"application/x-www-form-urlencoded"
+        },
+        data: querystring.stringify(this.form)
+      }).then((response)=>{
+                //请求结束 模态框关闭
+                this.closeModalHandler();
+                //提示消息
+                this.$message({
+                    type:"success",
+                    message:response.message
+                })
+            })
+
+    },
+    //重载员工数据
+    loadData(){
+      let url = "http://localhost:6677/waiter/findAll"
+      request.get(url).then((response)=>{
+        //箭头函数中的this指向外部函数中的this
+        this.employees = response.data;
+
+      })
+    },
+    toAddHandler(){
+            this.form={
+                type:"employee"
+            }
+            this.visible = true;
+        },
+    closeModalHandler(){
+      this.visible = false;
+    },
+    toDeleteHandler(id){
+      this.$confirm('此操作将永久删除该文件，是否继续？','提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(() => {
+              let url="http://localhost:6677/customer/deleteById?id="+id;
+              request.get(url).then((response)=>{
+                this.loadData();
+                this.$message({
+                    type: 'success',
+                    message:response.message
+        
+
+              });
+              
+                })
+                this.$message({
+                  type:"success",
+                  message: '删除成功！'+id
+                });
+            })
+    },
+    toUpdateHandler(row){
+      this.form=row;
+  
+      this.visible=true;
+
+    }
+  }
+}
+
+</script>
+
+<style >
+ 
+</style>
